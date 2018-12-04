06be15199f0c76cc4773412f16ad95f088355ce3
André Pascoal Bento <andrebento1995@sapo.pt>
2018-12-04 15:52:42 +0000

    Modified gitignore.

--- DayliStudent_BackEnd/daylistudent/server.py
+++ DayliStudent_BackEnd/daylistudent/server.py
@@ -47,7 +47,7 @@ def postGroup():
     rows_changed = execute_insert_query(insert_query)
     if rows_changed > 0:
         logger.info('Group created with name: {}'.format(group_name))
-        query = 'SELECT id FROM Groups WHERE name={};'.format(group_name)
+        query = 'SELECT (id) FROM Groups WHERE name={};'.format(group_name)
         logger.info('query:', query)
         group_id = execute_select_query(query)
         logger.info('Group created with id: {}'.format(group_id))
